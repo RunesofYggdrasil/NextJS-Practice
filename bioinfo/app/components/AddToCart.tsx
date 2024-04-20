66d24badd253e8404f4652d20ae9d7dcda493482
RunesofYggdrasil <rookmarcus79@gmail.com>
2024-04-20 15:22:49 -0400

    Practice

---
+++ bioinfo/app/components/AddToCart.tsx
@@ -0,0 +1,19 @@
+"use client";
+import React from "react";
+
+const AddToCart = () => {
+  return (
+    <div>
+      <button
+        type="button"
+        onClick={() => {
+          console.log("Click");
+        }}
+      >
+        A
+      </button>
+    </div>
+  );
+};
+
+export default AddToCart;
---
+++ bioinfo/app/components/GeneCard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const GeneCard = () => {
+  return (
+    <>
+      <div>
+        <h1>get from props</h1>
+      </div>
+    </>
+  );
+};
+
+export default GeneCard;
